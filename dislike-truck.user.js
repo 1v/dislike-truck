4579ad355ef8a8ae9c2d9e2db1cf5dd49e7d9d75
Artem <1v@users.noreply.github.com>
2016-10-13 21:04:57 +0300

    Version bump

--- dislike-truck.user.js
+++ dislike-truck.user.js
@@ -6,7 +6,7 @@
 // @namespace       dislike-truck
 // @description     Обращайтесь - https://github.com/1v/dislike-truck
 // @include         /^https?:\/\/(www\.|)youtube\.com[/]+[\s\S]*$/
-// @version         1.5
+// @version         1.5.1
 // @author          1v
 // @grant           none
 // @icon            http://img-fotki.yandex.ru/get/17846/203537249.14/0_1356dd_5dfe78f0_orig.png
